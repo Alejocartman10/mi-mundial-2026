e4767d97a74659ff3609bb4a86628884a98210b9
Alejocartman10 <alejocartman@gmail.com>
2026-06-12 08:01:37 -0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   let footballContext = '';
   try {
     const fixturesRes = await fetch(
-      'https://v3.football.api-sports.io/leagues?name=FIFA%20World%20Cup&type=Cup',
+      'https://v3.football.api-sports.io/fixtures?league=1&season=2026&next=10',
       {
         headers: {
           'x-apisports-key': process.env.API_FOOTBALL_KEY
